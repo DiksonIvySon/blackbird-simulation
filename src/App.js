dee3d0b5fd3fbf2f22e3fc2fdd258ebe63f3009f
DiksonIvySon <120308018+DiksonIvySon@users.noreply.github.com>
2025-10-27 19:53:51 +0200

    Updated logo and link to Propeller Aero DirtMate

--- src/App.js
+++ src/App.js
@@ -5,17 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        <img src="url" className="App-logo" alt="logo" />
         <p>
           Edit <code>src/App.js</code> and save to reload.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://www.propelleraero.com/dirtmate/"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Learn DirtMate
         </a>
       </header>
     </div>
